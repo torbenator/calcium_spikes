22bde65f11e0b049179934bb51b25e9c1a0f7235
Vivek Sagar <VivekSagar2016@u.northwestern.edu>
2017-06-01 19:43:56 -0500

    RNN

---
+++ raw.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  1 00:53:25 2017
+
+@author: viveksagar
+"""
+
+import tensorflow as tf
+from tensorflow.python.ops import rnn, rnn_cell
+import numpy as np
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+
+def create_attributes(calcium, spikes, num_history = 5, num_fut = 5, offset_step=5, num_der = 2, integ_kernel = 100):
+# num_history = number of points in the past used as attribute, 
+# num_fut = number of points in the future taken as attribute, offset_step = distance between history/future points
+# num_der = maximum order of derivative, integ_kernel  = window size of running cumsum 
+    print("Creating additional attributes...")
+    cal_past = np.asarray([np.roll(np.squeeze(calcium),(ii+1)*offset_step) for ii in range(num_history)]).T # History
+    cal_fut = np.asarray([np.roll(np.squeeze(calcium),-(ii+1)*offset_step) for ii in range(num_fut)]).T # Future
+    cal_grad = np.asarray([np.append(np.diff(np.squeeze(calcium),ii+1),np.zeros(ii+1)) for ii in range(num_der)]).T # n-Gradients upto n-order      
+    cal_integ =  np.cumsum(calcium) # Cum-sum
+    cal_integ = (cal_integ[integ_kernel:]-cal_integ[:-integ_kernel])/integ_kernel # Moving Avg
+    cal_avg =  np.asarray([np.lib.pad(cal_integ, (len(calcium)-len(cal_integ),0), 'edge')]).T # Padded with additional values to maintain original size   
+    att_data_raw = np.concatenate((calcium,cal_past,cal_fut,cal_grad, cal_avg), axis = 1)     
+    mask = np.ones(len(att_data_raw),dtype=bool) # Clip the edges with inaccurate attributes
+    mask[:integ_kernel], mask[-num_fut*offset_step:] = False,False
+    att_data = att_data_raw[mask]
+    clipped_spikes = spikes[mask].astype(np.float64)    
+    return att_data, clipped_spikes
+    
+def one_hot_label(label):
+# One hot representation of labels for cross entropy measurement
+    print("Using onehot representation")
+    label_onehot = np.concatenate((label,1-label),axis=1)
+    return label_onehot
+    
+def pre_train(att_data,clipped_spikes,batch_size):
+#     Basic preprocessing prior to training: scaling, crossvalidation split, clipping the size to match batch size.
+    att_data = preprocessing.scale(att_data)
+    train_x, test_x, train_y, test_y = train_test_split(att_data, clipped_spikes, test_size=0.2)
+    split_tr = (len(train_x)-np.mod(len(train_x),batch_size)).astype(np.int64)
+    split_test = (len(test_x)-np.mod(len(test_x),batch_size)).astype(np.int64) 
+    mask_tr = np.ones(len(train_x),dtype=bool) # Clip the edges to have the len of data a multiple of batch_size
+    mask_test = np.ones(len(test_x),dtype=bool)
+    mask_tr[split_tr:], mask_test[split_test:] = False,False
+    train_x = train_x[mask_tr]
+    train_y = train_y[mask_tr]
+    train_y = one_hot_label(train_y)
+    test_x = test_x[mask_test]
+    test_y = test_y[mask_test]
+    test_y = one_hot_label(test_y)
+    return train_x, test_x, train_y, test_y
+    
+# RNN
+    
+hm_epochs = 10
+n_classes = 2
+batch_size = 128
+chunk_size = 7
+n_chunks = 4
+rnn_size = 256
+
+x = tf.placeholder('float', [None, n_chunks,chunk_size])
+y = tf.placeholder('float')
+
+def recurrent_neural_network(x):
+    layer = {'weights':tf.Variable(tf.random_normal([rnn_size,n_classes])),
+             'biases':tf.Variable(tf.random_normal([n_classes]))}
+    x = tf.transpose(x, [1,0,2])
+    x = tf.reshape(x, [-1, chunk_size])
+    x = tf.split(0, n_chunks, x)
+    lstm_cell = rnn_cell.BasicLSTMCell(rnn_size,state_is_tuple=True)
+    outputs, states = rnn.rnn(lstm_cell, x, dtype=tf.float32)
+    output = tf.matmul(outputs[-1],layer['weights']) + layer['biases']
+    return output
+    
+def train_neural_network(att_data, clipped_spikes):
+    [train_x, test_x, train_y, test_y]=pre_train(att_data,clipped_spikes,batch_size)
+    prediction = recurrent_neural_network(x)
+    cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(prediction,y) )
+    optimizer = tf.train.AdamOptimizer().minimize(cost)
+    with tf.Session() as sess:
+        sess.run(tf.initialize_all_variables())
+        for epoch in range(hm_epochs):
+            epoch_loss = 0
+            i=0
+            while i < len(train_x):
+                start = i
+                end = i+batch_size
+                batch_x = np.array(train_x[start:end])
+                batch_x = batch_x.reshape((batch_size/2,n_chunks,chunk_size))
+                batch_y = np.array(train_y[start:end])
+                _, c = sess.run([optimizer, cost], feed_dict={x: batch_x, y: batch_y})
+                epoch_loss += c
+                i+=batch_size
+            print('Epoch', epoch+1, 'completed out of ', hm_epochs,'loss:',epoch_loss)
+            spike_predict= prediction.eval({x:test_x})
+    tf.reset_default_graph()
+    return test_y, spike_predict            
