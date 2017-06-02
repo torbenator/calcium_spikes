defd3dbdda5c2e73b642ceca78f217911ce53295
Vivek Sagar <VivekSagar2016@u.northwestern.edu>
2017-06-02 02:30:08 -0500

    RNN corrected. Up and running.

--- raw.py
+++ raw.py
@@ -6,12 +6,12 @@ Created on Thu Jun  1 00:53:25 2017
 """
 
 import tensorflow as tf
-from tensorflow.python.ops import rnn, rnn_cell
+from tensorflow.contrib import rnn
 import numpy as np
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 
-def create_attributes(calcium, spikes, num_history = 5, num_fut = 5, offset_step=5, num_der = 2, integ_kernel = 100):
+def create_attributes(calcium, spikes, num_history = 45, num_fut = 1, offset_step=1, num_der = 2, integ_kernel = 100):
 # num_history = number of points in the past used as attribute, 
 # num_fut = number of points in the future taken as attribute, offset_step = distance between history/future points
 # num_der = maximum order of derivative, integ_kernel  = window size of running cumsum 
@@ -57,9 +57,9 @@ def pre_train(att_data,clipped_spikes,batch_size):
 hm_epochs = 10
 n_classes = 2
 batch_size = 128
-chunk_size = 7
-n_chunks = 4
-rnn_size = 256
+chunk_size = 5
+n_chunks = 10
+rnn_size = 64
 
 x = tf.placeholder('float', [None, n_chunks,chunk_size])
 y = tf.placeholder('float')
@@ -67,21 +67,21 @@ y = tf.placeholder('float')
 def recurrent_neural_network(x):
     layer = {'weights':tf.Variable(tf.random_normal([rnn_size,n_classes])),
              'biases':tf.Variable(tf.random_normal([n_classes]))}
-    x = tf.transpose(x, [1,0,2])
-    x = tf.reshape(x, [-1, chunk_size])
-    x = tf.split(0, n_chunks, x)
-    lstm_cell = rnn_cell.BasicLSTMCell(rnn_size,state_is_tuple=True)
-    outputs, states = rnn.rnn(lstm_cell, x, dtype=tf.float32)
+    x = tf.unstack(x, n_chunks, 1)
+    lstm_cell = rnn.BasicLSTMCell(rnn_size,forget_bias=1.0)
+    
+    # Get lstm cell output
+    outputs, states = rnn.static_rnn(lstm_cell, x, dtype=tf.float32)
     output = tf.matmul(outputs[-1],layer['weights']) + layer['biases']
     return output
     
 def train_neural_network(att_data, clipped_spikes):
     [train_x, test_x, train_y, test_y]=pre_train(att_data,clipped_spikes,batch_size)
     prediction = recurrent_neural_network(x)
-    cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(prediction,y) )
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y))
     optimizer = tf.train.AdamOptimizer().minimize(cost)
     with tf.Session() as sess:
-        sess.run(tf.initialize_all_variables())
+        sess.run(tf.global_variables_initializer())
         for epoch in range(hm_epochs):
             epoch_loss = 0
             i=0
@@ -89,12 +89,20 @@ def train_neural_network(att_data, clipped_spikes):
                 start = i
                 end = i+batch_size
                 batch_x = np.array(train_x[start:end])
-                batch_x = batch_x.reshape((batch_size/2,n_chunks,chunk_size))
+                batch_x = batch_x.reshape((batch_size,n_chunks,chunk_size))
                 batch_y = np.array(train_y[start:end])
                 _, c = sess.run([optimizer, cost], feed_dict={x: batch_x, y: batch_y})
                 epoch_loss += c
                 i+=batch_size
             print('Epoch', epoch+1, 'completed out of ', hm_epochs,'loss:',epoch_loss)
+            test_x = test_x.reshape((-1,n_chunks,chunk_size))
             spike_predict= prediction.eval({x:test_x})
     tf.reset_default_graph()
     return test_y, spike_predict            
+
+def main(calcium,spikes):
+#    Provide inputs as 1D column vector of shape (N,1)
+    [att_data,clipped_spikes] = create_attributes(calcium,spikes)
+    [test_spikes, predicted_spikes]= train_neural_network(att_data,clipped_spikes)
+    return test_spikes, predicted_spikes
+    
